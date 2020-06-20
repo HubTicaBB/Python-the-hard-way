36e1a1146cfd6322eddabe482b7db871dfdca850
Tijana <tijana.ilic87@protonmail.com>
2020-06-21 00:15:34 +0200

    Updatae More Files

--- More_Files.py
+++ More_Files.py
@@ -9,16 +9,12 @@ print(f"Copying from {from_file} to {to_file}")
 in_file = open(from_file)
 indata = in_file.read()
 
-print(f"The input file is {len(indata)} bytes long")
+print(f"{from_file}'s content:\n{len(indata)} bytes\n\n{indata}")
 
-print(f"Does the output file exist? {exists(to_file)}")
-print("Ready, hit ENTER to continue, CTRL-C to abort.")
-input()
+print(f"Output file exist? {exists(to_file)}")
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
-
-print("Alright, all done")
 
 out_file.close()
 in_file.close()
